f82e001a7dbf667313691d74230840325ebb1432
Kin77Dragg <106829078+Kin77Dragg@users.noreply.github.com>
2024-03-01 12:27:38 +0700

    Add files via upload

---
+++ formcekpermohonan.php
@@ -0,0 +1,16 @@
+<?php
+	include('koneksi.db.php');
+	if(isset($_POST['NomorSurat'])){
+		$NomorSurat=filter_var($_POST['NomorSurat'],FILTER_SANITIZE_STRING);
+		$sql="SELECT * FROM `headersurat` WHERE NomorSurat='".$NomorSurat."'";
+		$q=mysqli_query($koneksi,$sql);
+		$r=mysqli_fetch_array($q);
+		if (empty($r)){
+			echo "<script>
+			alert('Permohonan Baru'); window.location.href='formpermohonankosong.php';</script>";
+		}else{
+			echo "<script>
+			alert('Permohonan Ditemukan'); window.location.href='formpermohonanisi.php';</script>";
+		}
+	}
+?>
