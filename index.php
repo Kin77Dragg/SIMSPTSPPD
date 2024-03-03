d480aa0c3cfdcb5f883e649c25973b84309a2851
M. Ikhwan Fauzi <114634221+Ikhwanfauzi02@users.noreply.github.com>
2024-03-03 12:21:54 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -139,12 +139,12 @@ exit();
 									<a class="nav-link" href="vakabag.php" target="frmmain">validasi kabag</a>									
                                 </nav>
                             </div>
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts1" aria-expanded="false" aria-controls="collapseLayouts">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                                 Cetak Surat
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                            <div class="collapse" id="collapseLayouts1" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
                                     <a class="nav-link" href="anggotadewan.php" target="frmmain">Anggota Dewan</a>
                                     <a class="nav-link" href="Pendamping.php" target="frmmain">Pendamping</a>
